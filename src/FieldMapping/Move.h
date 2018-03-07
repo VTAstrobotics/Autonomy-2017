df57054b6ce944b234195a08f6ee0b4a12c200fd
bradysm <bradysm@vt.edu>
2018-03-06 22:50:41 -0500

    added functionality to the move class

--- src/FieldMapping/Move.h
+++ src/FieldMapping/Move.h
@@ -4,11 +4,15 @@
 // right means to the right if the robot is facing mining area
 enum Direction {RIGHT, LEFT, TOWARDS_MINE, TOWARDS_BIN};
 
-class Move{
+/*
+This class will define the next move of the robot. We will
+gain access to the map by inheriting from the Map class
+*/
+class Move : Map{
 private:
   Direction direction; // direction of the move
   double distance;
-
+  void distance(int robotRow, int robotCol, int mineRow,int mineCol);
 public:
   Move(); // constructor
 
@@ -19,6 +23,7 @@ public:
   // mutator methods
   Direction getDirection();
   double getDistance();
+  void choseMove(); // decides on which direction to move in
 
 };
 
